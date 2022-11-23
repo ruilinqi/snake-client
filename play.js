7bdcfca076a69de01a9cbb245f5e8f8cd43ddab6
ruilinqi <ruilinqi7@gmail.com>
2022-11-23 18:34:58 +0000

    Separated client file and main play file

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
+const connect = require("./client");
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541, // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data); //a message sent from the server
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
