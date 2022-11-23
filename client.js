90fab9ac21a78468cf00ea29ba260ee8c8c92e86
ruilinqi <ruilinqi7@gmail.com>
2022-11-23 21:40:54 +0000

    Implemented files

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 connect = () => {
   const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541, // PORT number here,
+    host: IP, // IP address here,
+    port: PORT, // PORT number here,
   });
 
   // interpret incoming data as text
@@ -15,35 +16,40 @@ connect = () => {
   });
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
+    
   });
   conn.on("connect", () => {
-    let timer = 100;
-    for (let count = 0; count < 20; count++){
-      setTimeout(() => {
-        conn.write("Move: up"), timer}
-        , timer += 50)
-    }
-      // setTimeout(() => {
-      //   conn.write("Move: left");
-      // }, 400)
-      // setTimeout(() => {
-      //   conn.write("Move: up");
-      // }, 600)
-      // setTimeout(() => {
-      //   conn.write("Move: left");
-      // }, 800)
-      // setTimeout(() => {
-      //   conn.write("Move: up");
-      // }, 1000)
-      // setTimeout(() => {
-      //   conn.write("Move: left");
-      // }, 1200)
-
+    conn.write("Name: RQI");
   });
+ 
+  // conn.on("connect", () => {
+  //   //let timer = 100;
+  //   // for (let count = 0; count < 20; count++){
+  //   //   setTimeout(() => {
+  //   //     conn.write("Move: up"), timer}
+  //   //     , timer += 50)
+  //   // }
+  //     // setTimeout(() => {
+  //     //   conn.write("Move: left");
+  //     // }, 400)
+  //     // setTimeout(() => {
+  //     //   conn.write("Move: up");
+  //     // }, 600)
+  //     // setTimeout(() => {
+  //     //   conn.write("Move: left");
+  //     // }, 800)
+  //     // setTimeout(() => {
+  //     //   conn.write("Move: up");
+  //     // }, 1000)
+  //     // setTimeout(() => {
+  //     //   conn.write("Move: left");
+  //     // }, 1200)
+
+  // });
 
   // process.stdin.on('data', (userInput) => {
   //   conn.write(userInput);
   // })
   return conn;
 };
-module.exports = connect;
+module.exports = { connect };
